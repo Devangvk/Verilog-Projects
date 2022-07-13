8ce5ade03b523583db1015d401ae77d1e4ef3d57
Devang Khetia <58927723+Devangvk@users.noreply.github.com>
2022-07-13 13:12:44 +0530

    Add files via upload

---
+++ Half Adder/Hatb.v
@@ -0,0 +1,21 @@
+`include "Ha.v"
+`timescale 1ns/1ps
+module Hatb();
+reg atb,btb;
+wire stb,ctb;
+Ha DUT (
+atb,btb,stb,ctb
+);
+initial
+	begin
+	$monitor($time,"atb=%b,btb=%b,stb=%b,ctb=%b",atb,btb,stb,ctb);
+	$dumpfile("Ha.vcd");
+	$dumpvars(0,Hatb);
+	atb=1'b0;btb=1'b0;#10;
+	atb=1'b0;btb=1'b1;#10;
+	atb=1'b1;btb=1'b0;#10;
+	atb=1'b1;btb=1'b1;#10
+	#10;
+	$finish;
+	end
+endmodule
